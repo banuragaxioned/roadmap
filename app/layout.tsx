128aab6a86273c7e4af3f83d4fbb7ce5a7a92455
Anurag Banerjee <anurag@axioned.com>
2023-11-26 00:25:44 +0530

    Tidy up

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
 					<div className="flex">
-						<div className="w-56"></div>
 						<div>{children}</div>
 					</div>
 				</ThemeProvider>
